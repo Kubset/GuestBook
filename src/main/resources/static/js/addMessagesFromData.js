1c080d619ee5693d8f5a86d80f716cfeed2fe1d1
Kubset <kuba1228334@gmail.com>
2018-06-13 09:30:33 +0200

    add styles to html

--- src/main/resources/static/js/addMessagesFromData.js
+++ src/main/resources/static/js/addMessagesFromData.js
@@ -12,3 +12,34 @@ async function addMessage() {
   val.innerHTML += messageBody;
 }
 
+async function createDivBodyFromJson(jsonPath) {
+    const divBody = document.getElementById("messages");
+    const messagesJson = await getJsonFromPath(jsonPath);
+
+    if (messagesJson.messages.length === 0) {
+      return null;
+    }
+     addToBody(messagesJson, divBody);
+}
+
+async function getJsonFromPath(path) {
+    const response = await fetch(path);
+    return response.json();
+}
+
+function addToBody(messagesJson, divBody) {
+
+  for (let i = 0; i < messagesJson.messages.length; i++) {
+    const message = messagesJson.messages[i];
+
+    const messageBody = '<div class="message-element">' +
+                    '<h4>' + 'Name:' + message.name + '</h4>' +
+                    '<p>' + 'Message: '+ message.message + '</p>' +
+                    '<p>' + 'Data: ' + message.data + '</p>' +
+                    '</div>';
+
+      divBody.innerHTML += messageBody;
+  }
+}
+
+createDivBodyFromJson("static/data/exampleData.json");
