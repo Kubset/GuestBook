52025dba4a2da092b67800aadb7589256d356c2d
Kubset <kuba1228334@gmail.com>
2018-06-14 22:07:12 +0200

    Major refactor of code

--- src/main/java/GuestBook/App.java
+++ src/main/java/GuestBook/App.java
@@ -4,19 +4,10 @@ import GuestBook.messageDAO.ConnectionProvider;
 import GuestBook.messageDAO.InterfaceDAO;
 import GuestBook.messageDAO.MessageDAO;
 import GuestBook.messageDAO.Models.Message;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.sun.net.httpserver.HttpServer;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -28,6 +19,7 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(7171), 0);
 
         // set routes
+        //Universal type of site is bad practise, now I know that
         server.createContext("/hello", new OpenSite("index.html"));
         server.createContext("/static", new Static());
         server.setExecutor(null); // creates a default executor
